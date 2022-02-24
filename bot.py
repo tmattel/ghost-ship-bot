77735051d25f909b3507e65079ded4db6b3417ad
tmattel <77650997+tmattel@users.noreply.github.com>
2022-02-24 00:07:06 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,6 +15,13 @@ some_api_token = os.environ['SOME_API_TOKEN']
 r = redis.from_url(os.environ.get("REDIS_URL"))
 
 #       Your bot code below
-# bot = telebot.TeleBot(token)
-# some_api = some_api_lib.connect(some_api_token)
-#              ...
+bot = telebot.TeleBot(token)
+some_api = some_api_lib.connect(some_api_token)
+
+keyboard1 = telebot.types.ReplyKeyboardMarkup()
+keyboard1.row('Wallet', 'Buy Postage')
+
+keyboard2 = telebot.types.ReplyKeyboardMarkup()
+keyboard2.row('Settings', 'Support')
+
+ReplyKeyboardMarkup = keyboard1.resize_keyboard, keyboard2.resize_keyboard  = True, True
